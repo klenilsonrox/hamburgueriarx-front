ad4f46eb3ca0099a93a355373d8fc3c1c5ec3d0e
klenilsonrox <klenilsoncoc@gmail.com>
2024-10-31 19:32:28 -0300

    att

--- src/app/(paginas)/conta/dados-cadastrais/page.js
+++ src/app/(paginas)/conta/dados-cadastrais/page.js
@@ -1,11 +1,15 @@
 import Dados from '@/app/components/Dados';
+import Header from '@/app/components/Header';
 import React from 'react';
 
 const page = () => {
   return (
+    <>
+    <Header />
     <div className="p-4 max-w-7xl mx-auto min-h-screen">
       <Dados />
     </div>
+    </>
   );
 };
 
