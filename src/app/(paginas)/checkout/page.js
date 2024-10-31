82ee59643a52f708962550819c3ffc0e16679c43
klenilsonrox <klenilsoncoc@gmail.com>
2024-10-31 19:20:11 -0300

    ARTR

--- src/app/(paginas)/checkout/page.js
+++ src/app/(paginas)/checkout/page.js
@@ -1,11 +1,13 @@
 import Checkout from '@/app/components/Checkout';
 import Container from '@/app/components/Container';
+import Header from '@/app/components/Header';
 import { CartProvider } from '@/app/contexts/CartContext';
 import React from 'react';
 
 const page = () => {
   return (
    <>
+   <Header />
    <Container>
     <CartProvider>
         <Checkout />
