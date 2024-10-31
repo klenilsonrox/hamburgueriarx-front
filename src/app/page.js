44a6f65c1ff0a4f960e2f4f90e4a392486bd1b12
klenilsonrox <klenilsoncoc@gmail.com>
2024-10-31 19:52:05 -0300

    att

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import Link from 'next/link';
+import Footer from './components/Footer';
 
 const page = () => {
   return (
@@ -12,13 +13,14 @@ const page = () => {
     <div className='lg:mt-20 max-w-[600px]'>
       <h1 className='text-[40px] lg:text-[60px] font-bold text-gray-600 flex after:absolute after:w-10 after:h-3 after:bg-red-600 after:block'>HamburgueriaRx<span className='text-red-600'>.</span> </h1>
       <p className='text-gray-700'>HamburgueriaRx é a hamburgueria que traz a combinação perfeita de sabor, qualidade e inovação. Nosso cardápio é cuidadosamente elaborado para oferecer hambúrgueres artesanais suculentos, preparados com ingredientes frescos e selecionados. Seja para um almoço rápido, um jantar com amigos ou uma entrega no conforto de casa, a Rx Burguers garante uma experiência gastronômica única.!</p>
-    <Link href="/produtos/sanduiches" className='bg-red-600 py-3 px-8 rounded-md text-white mt-4 block max-w-[130px] text-center'>Cardápio</Link>
+    <Link href="/cardapio" className='bg-red-600 py-3 px-8 rounded-md text-white mt-4 block max-w-[130px] text-center'>Cardápio</Link>
     </div>
     <div>
       <img src="/artesanal.avif" alt="imagem sanduiche"/>
     </div>
     </div>
     </div>
+    <Footer />
    </>
   );
 };
