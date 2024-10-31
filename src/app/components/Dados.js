b955616b079a92d425532e01ef49cf8aa4e963e8
klenilsonrox <klenilsoncoc@gmail.com>
2024-10-31 19:49:39 -0300

    att

--- src/app/components/Dados.js
+++ src/app/components/Dados.js
@@ -20,10 +20,14 @@ const Dados = () => {
     const [complemento, setComplemento] = useState("");
     const [referencia, setReferencia] = useState("");
     const [loading, setLoading] = useState(false);
+    const router = useRouter()
+ 
 
 
     async function getUser() {
+     try {
         const token = await getToken();
+        setLoading(true)
         const response = await fetch('/api/infos', {
             next: { revalidate: 0 },
             method: 'GET',
@@ -32,8 +36,19 @@ const Dados = () => {
                 "Authorization": `Bearer ${token}`
             },
         });
+
+   
+        if(response.status===400){
+            router.push('/auth/entrar')
+        }
+
         const data = await response.json();
         setUser(data);
+     } catch (error) {
+        console.log(error)
+     }finally{
+        setLoading(false)
+     }
     }
 
     useEffect(() => {
@@ -111,38 +126,42 @@ const Dados = () => {
     };
 
     return (
-        <CartProvider>
-          <div className='rounded-lg p-6 flex flex-col items-center'>
-            <ToastContainer />
-            <h1 className='text-2xl font-bold text-center mb-6'>Atualizar Dados</h1>
-            <form className='w-full max-w-lg' onSubmit={handleSubmit}>
-                <Input label="Nome Completo" id="name" value={name} setState={setName} className="bg-white"/>
-                <div className='flex flex-col lg:flex-row gap-2'>
-                    <Input label="Email" id="email" value={user.email} setState={setWhatsapp} className="bg-gray-100 disabled:cursor-not-allowed" disabled/>
-                    <Input label="WhatsApp" id="whatsapp" value={whatsapp} setState={setWhatsapp} className="bg-white"/>
-                </div>
-                <div className='flex flex-col lg:flex-row gap-2'>
-                    <Input label="Cep" id="cep" value={cep} setState={setCep} className="bg-white"/>
-                    <Input label="Rua" id="rua" value={rua} setState={setRua} className="bg-white"/>
-                </div>
-                <div className='flex flex-col lg:flex-row gap-2'>
-                    <Input label="Número" id="numero" value={numero} setState={setNumero} className="bg-white"/>
-                    <Input label="Bairro" id="bairro" value={bairro} setState={setBairro} className="bg-white"/>
-                </div>
-                <div className='flex flex-col lg:flex-row gap-2'>
-                    <Input label="Cidade" id="cidade" value={cidade} setState={setCidade} className="bg-white"/>
-                    <Input label="Complemento" id="complemento" value={complemento} setState={setComplemento} className="bg-white"/>
-                </div>
-                <Input label="Referência" id="referencia" value={referencia} setState={setReferencia} className="bg-white"/>
-                <div className='mt-4 flex items-center justify-between'>
-                    <Link href="/cardapio" className='text-red-600 hover:text-red-500'>Voltar</Link>
-                    <button type="submit" className='bg-red-600 hover:bg-red-400 px-6 py-3 rounded-md text-white transition duration-200' disabled={loading}>
-                        {loading ? "Carregando..." : "Salvar"}
-                    </button>
-                </div>
-            </form>
-        </div>
-        </CartProvider>
+        <>
+        {loading ? <p>Carregando...</p> : (
+            <CartProvider>
+            <div className='rounded-lg p-6 flex flex-col items-center'>
+              <ToastContainer />
+              <h1 className='text-2xl font-bold text-center mb-6'>Atualizar Dados</h1>
+              <form className='w-full max-w-lg' onSubmit={handleSubmit}>
+                  <Input label="Nome Completo" id="name" value={name} setState={setName} className="bg-white"/>
+                  <div className='flex flex-col lg:flex-row gap-2'>
+                      <Input label="Email" id="email" value={user.email} setState={setWhatsapp} className="bg-gray-100 disabled:cursor-not-allowed" disabled/>
+                      <Input label="WhatsApp" id="whatsapp" value={whatsapp} setState={setWhatsapp} className="bg-white"/>
+                  </div>
+                  <div className='flex flex-col lg:flex-row gap-2'>
+                      <Input label="Cep" id="cep" value={cep} setState={setCep} className="bg-white"/>
+                      <Input label="Rua" id="rua" value={rua} setState={setRua} className="bg-white"/>
+                  </div>
+                  <div className='flex flex-col lg:flex-row gap-2'>
+                      <Input label="Número" id="numero" value={numero} setState={setNumero} className="bg-white"/>
+                      <Input label="Bairro" id="bairro" value={bairro} setState={setBairro} className="bg-white"/>
+                  </div>
+                  <div className='flex flex-col lg:flex-row gap-2'>
+                      <Input label="Cidade" id="cidade" value={cidade} setState={setCidade} className="bg-white"/>
+                      <Input label="Complemento" id="complemento" value={complemento} setState={setComplemento} className="bg-white"/>
+                  </div>
+                  <Input label="Referência" id="referencia" value={referencia} setState={setReferencia} className="bg-white"/>
+                  <div className='mt-4 flex items-center justify-between'>
+                      <Link href="/cardapio" className='text-red-600 hover:text-red-500'>Voltar</Link>
+                      <button type="submit" className='bg-red-600 hover:bg-red-400 px-6 py-3 rounded-md text-white transition duration-200' disabled={loading}>
+                          {loading ? "Carregando..." : "Salvar"}
+                      </button>
+                  </div>
+              </form>
+          </div>
+          </CartProvider>
+        )}
+        </>
     );
 };
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import Header from './components/Header';
+import Link from 'next/link';
 
 const page = () => {
   return (
     
-   <>
+   <> 
     <Header />
-    <div className="p-4 max-w-7xl mx-auto">
-     
-      <h1>inicio</h1>
+    <div className='w-full bg-white min-h-screen flex items-center justify-center'>
+      <div className="p-4 max-w-7xl mx-auto bg-white grid lg:grid-cols-2 items-start lg:pt-20 animaLeft">
+    <div className='lg:mt-20 max-w-[600px]'>
+      <h1 className='text-[40px] lg:text-[60px] font-bold text-gray-600 flex after:absolute after:w-10 after:h-3 after:bg-red-600 after:block'>HamburgueriaRx<span className='text-red-600'>.</span> </h1>
+      <p className='text-gray-700'>HamburgueriaRx é a hamburgueria que traz a combinação perfeita de sabor, qualidade e inovação. Nosso cardápio é cuidadosamente elaborado para oferecer hambúrgueres artesanais suculentos, preparados com ingredientes frescos e selecionados. Seja para um almoço rápido, um jantar com amigos ou uma entrega no conforto de casa, a Rx Burguers garante uma experiência gastronômica única.!</p>
+    <Link href="/produtos/sanduiches" className='bg-red-600 py-3 px-8 rounded-md text-white mt-4 block max-w-[130px] text-center'>Cardápio</Link>
+    </div>
+    <div>
+      <img src="/artesanal.avif" alt="imagem sanduiche"/>
+    </div>
+    </div>
     </div>
    </>
   );
