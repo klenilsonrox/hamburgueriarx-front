98a0c7aa86339bc685fd16367aff2217fdc89f01
klenilsonrox <klenilsoncoc@gmail.com>
2024-11-01 20:55:43 -0300

    att

--- src/app/(paginas)/admin/pedidos/page.js
+++ src/app/(paginas)/admin/pedidos/page.js
@@ -68,7 +68,7 @@ export default function OrderManagement() {
   }
 
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Pedidos</h1>
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
         {orders.map((order) => (
